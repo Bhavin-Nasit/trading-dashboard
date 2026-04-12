7d9508c7e46e72be9e97a232e5c7c0e7f4fe5d12
Bhavin Nasit <43333295+Bhavin-Nasit@users.noreply.github.com>
2026-04-12 11:23:50 +0530

    Update server.py

--- server.py
+++ server.py
@@ -399,4 +399,6 @@ if __name__ == "__main__":
     print("  UI   →  open index.html with Live Server")
     print("="*52 + "\n")
     get_nse()
-    app.run(host="0.0.0.0", port=8000, debug=False)
+    # app.run(host="0.0.0.0", port=8000, debug=False)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port, debug=False)
